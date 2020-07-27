f7e77dbac3c39ee600295474416fef719c1d59b7
Nicola Piermattei <pierma.nick@live.it>
2020-07-27 09:42:52 +0200

    Insertion Model Created

---
+++ Models/InsertModels/DailyInsert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NicolaPIermatteiWec.Models.InsertModels
+{
+    public class DailyInsert
+    {
+        [Required]
+        [MaxLength(50)]
+        public string DispoId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string DispoContactId { get; set; }
+        [Required]
+        public int Prox { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        public float Latitude { get; set; }
+        [Required]
+        public float Longitude { get; set; }
+        [Required]
+        public int Distance { get; set; }
+        [Required]
+        public DateTime DateContacts{ get; set; }
+    }
+}
---
+++ Models/InsertModels/PositiveInsert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NicolaPIermatteiWec.Models.InsertModels
+{
+    public class PositiveInsert
+    {
+        [Required]
+        public string DispoId { get; set; }
+        [Required]
+        public bool Positive { get; set; }
+        [Required]
+        public DateTime DatePositive { get; set; }
+    }
+}
